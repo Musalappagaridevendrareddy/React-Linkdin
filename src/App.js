189f52a27d2f31811028e6436659074acaf3dd7c
DevendarDev <devsobhaeswar143@gmail.com>
2023-04-12 06:12:13 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
+    <div>
+      <nav>
+        <Link to="/Render">Render</Link>
+        <Link to="/Fetch">Fetch</Link>
+        <Link to="/Test">Test</Link>
+      </nav>
       <h1>Hello boxsand</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
